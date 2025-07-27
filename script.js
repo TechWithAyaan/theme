499e3b20bfddb408a968e23d02db255e67ee4944
Tech With Ayaan <techwithayaanfaheem@gmail.com>
2025-07-27 10:53:59 -0700

    Dark Light Theme

---
+++ script.js
@@ -0,0 +1,9 @@
+function dark() {
+    document.body.style.backgroundColor = "#212121";
+    document.body.style.color = "#e8e8e8";
+}
+
+function light() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+}
